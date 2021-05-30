eb2f330429d9f8c253307589d483740800705dd8
vikaskodag2 <vikaskodag2@gmail.com>
2021-05-30 15:01:35 +0530

    Patched up Trending page with pagination

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -2,30 +2,33 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 import MovieCard from "../../components/MovieCard";
+import CustomPagination from "../../components/Pagination";
 
 import "./Trending.css";
 
 const Trending = () => {
-  const [content, setContent] = useState([]);
-
-  const fetchTrending = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
-    );
-
-    setContent(data.results);
-  };
+  const [content, setContent] = useState({});
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    const fetchTrending = async () => {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+      );
+
+      setContent(data);
+    };
+
     fetchTrending();
-  }, []);
+  }, [page]);
 
   return (
     <div className="trendingPage">
       <span className="pageTitle">Trending</span>
       <div className="trendingContent">
         {content &&
-          content.map((movie) => (
+          content.results &&
+          content.results.map((movie) => (
             <MovieCard
               key={movie.id}
               id={movie.id}
@@ -37,6 +40,11 @@ const Trending = () => {
             />
           ))}
       </div>
+      <CustomPagination
+        totalPages={content.total_pages}
+        setPage={setPage}
+        curPage={page}
+      />
     </div>
   );
 };
