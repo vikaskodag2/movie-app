d2ec8a184c36f579d158e34bdc1ff56c8aed3ebc
vikaskodag2 <vikaskodag2@gmail.com>
2021-05-30 12:26:47 +0530

    Api call to fetch trending movies

---
+++ src/Pages/Trending/Trending.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Trending = () => {
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    console.log("data: ", data);
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
+  return (
+    <div>
+      <span className="pageTitle">Trending</span>
+    </div>
+  );
+};
+
+export default Trending;
--- src/components/Pages/Trending/Trending.js
+++
@@ -1,9 +0,0 @@
-const Trending = () => {
-  return (
-    <div>
-      <span className="pageTitle">Trending</span>
-    </div>
-  );
-};
-
-export default Trending;
