c7df1de7afce718b2ef71fd054bdb4dc46f96425
vikaskodag2 <vikaskodag2@gmail.com>
2021-05-30 17:31:17 +0530

    added movie content modal

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,12 @@
 import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
+import ContentModal from "../ContentModal";
 
 import "./MovieCard.css";
 
 const MovieCard = ({ id, title, poster, date, mediaType, voteAverage }) => {
   return (
-    <div id={id} className="movieCard">
+    <ContentModal id={id} className="movieCard" mediaType={mediaType}>
       <Badge
         badgeContent={voteAverage}
         color={voteAverage > 6 ? "primary" : "secondary"}
@@ -20,7 +21,7 @@ const MovieCard = ({ id, title, poster, date, mediaType, voteAverage }) => {
         {mediaType === "tv" ? "TV Series" : "Movie"}
         <span className="subTitle">{date}</span>
       </span>
-    </div>
+    </ContentModal>
   );
 };
 
